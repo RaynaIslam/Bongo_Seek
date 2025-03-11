d0bd43e7a7705200def449f36cf09f7e0274bc4c
Abstract Progermmer <AbstractProgermmer@example.com>
2025-03-11 14:11:32 +0600

    login page

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import Signup from './Components/Signup'
 import "./App.css"
-// import Login from './Pages/Login'
+import Login from './Components/login'
 
 function App() {
   return (
     <div className="min-h-screen bg-base-200 flex flex-col items-center justify-center p-4">
       <nav className="mb-8 space-x-4">
         <Link to="/Signup" className="btn btn-primary">সাইন আপ</Link>
-        {/* <Link to="/login" className="btn btn-secondary">লগইন</Link> */}
+        <Link to="/login" className="btn btn-secondary">লগইন</Link>
       </nav>
 
       <Routes>
         <Route path="/Signup" element={<Signup />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
         {/* Optional: Default route */}
         <Route path="*" element={<div className="text-xl text-gray-700">পৃষ্ঠাটি খুঁজে পাওয়া যায়নি</div>} />
       </Routes>
